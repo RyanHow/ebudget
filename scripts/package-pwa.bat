cd42e52eb361fd0f0938dc88021668ba6e289ea8
Ryan How <ryan@bitworks.com.au>
2017-02-12 20:02:09 +0800

    build updates

---
+++ scripts/package-pwa.bat
@@ -0,0 +1,17 @@
+xcopy /Y src\index_pwa.html www
+xcopy /Y src\favicon.ico www
+xcopy /Y src\assets\icon\* www
+del www\index.html
+move www\index_pwa.html www\index.html
+git -C ..\ebudget-pwa-dist pull
+attrib -h ..\ebudget-pwa-dist\.git
+move ..\ebudget-pwa-dist\.git tmp
+del /S /Q /Y /F ..\ebudget-pwa-dist\*
+rd /S /Q ..\ebudget-pwa-dist\assets
+rd /S /Q ..\ebudget-pwa-dist\build
+move tmp\.git ..\ebudget-pwa-dist
+attrib +h ..\ebudget-pwa-dist\.git
+xcopy /E /Y www\* ..\ebudget-pwa-dist\
+git -C ..\ebudget-pwa-dist add .
+git -C ..\ebudget-pwa-dist commit -a -m "Automatic Deploy"
+git -C ..\ebudget-pwa-dist push
