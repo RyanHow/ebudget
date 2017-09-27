384e58d6ea141e9f46d04e24420856c9d03ba898
Ryan How <ryan@zbit.net.au>
2017-09-27 23:16:09 +0800

    Removed comments

--- src/demo/demo-setup.ts
+++ src/demo/demo-setup.ts
@@ -36,17 +36,6 @@ export class DemoSetup {
         await this.fadeIn();
     }
 
-    // TODO: Add a command for database operations
-    // TODO: Clear the demo database on reset
-    // TODO: Demo-mode "hide" the app until the setup is complete (fade out and timer)... then unhide (fade in and timer)... Reset should be part of the "setup", so setup will reset first, then run the setup so reset and setup happen while faded - Maybe make reset and fade part of the setup commands? - or force have if manual intervention??...
-    // TODO: Invoke demo automatically on load (setup and optionally start, this will be postmessage) AND have it faded out so we don't get a flash of the standard loading (this will be query param)
-    // TODO: Demo Controls - pause/play, reset, auto loop, etc... (maybe autoloop should be a player setup param, NOT a queue command ?)
-    // TODO: Input "Blocker" over it - not sure about keyboard focus... maybe block the entire iframe (so this could / would be outside of the app)
-    // TODO: Some way to deny the focus of the iFrame no idea how to do that... Otherwise text fields get focus and will wreak havoc with keyboard input... maybe remove autofocus somehow from all the html and any calls to .focus... it will mess with CSS, but that is ok... have this as an optional thing?
-
-    // TODO: Navigation is probably best done by deep-linking, otherwise all classes we nav to will need to be imported here and mapped from strings (Which you'd except, but would be good to minimise that) - or we set up a registry...
-
-
     async executeScript() {
         if (this.currentLine >= this.script.length) return;
 
